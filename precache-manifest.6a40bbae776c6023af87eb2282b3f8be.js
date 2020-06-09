4d38a486adb01c3ea011e980970bf86f820ebb38
asalinas9 <salinas99@live.com>
2020-06-09 18:05:39 -0500

    Updates

---
+++ precache-manifest.6a40bbae776c6023af87eb2282b3f8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16800c43b2ab2ef832e6025ae3fc983f",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a17dceccccfbc08bc889",
+    "url": "/Clicky-Game/static/css/main.9a0d290a.chunk.css"
+  },
+  {
+    "revision": "bdb82f10d1e9d78eff44",
+    "url": "/Clicky-Game/static/js/2.d1249166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-Game/static/js/2.d1249166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a17dceccccfbc08bc889",
+    "url": "/Clicky-Game/static/js/main.19ce3ac4.chunk.js"
+  },
+  {
+    "revision": "28943688ad0891451581",
+    "url": "/Clicky-Game/static/js/runtime-main.325a4581.js"
+  }
+]);
